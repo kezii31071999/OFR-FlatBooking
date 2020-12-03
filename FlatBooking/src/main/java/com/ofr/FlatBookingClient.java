1129611cbd89b6ecd6637809f9ad732612dfd5ca
welcome <welcome@DESKTOP-A4CVPGI>
2020-12-03 19:15:44 +0530

    Second Commit

--- FlatBooking/src/main/java/com/ofr/FlatBookingClient.java
+++ FlatBooking/src/main/java/com/ofr/FlatBookingClient.java
@@ -22,5 +22,7 @@ public class FlatBookingClient {
 	public Docket productApi() {
 	return new Docket(DocumentationType.SWAGGER_2).select()
 	.apis(RequestHandlerSelectors.basePackage("com.ofr")).build();
+	
+	
 	}
 }
